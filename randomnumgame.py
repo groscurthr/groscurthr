7c57c1bea88bde217e623dd28e49a28630d1f43b
groscurthr <58827889+groscurthr@users.noreply.github.com>
2019-12-12 14:59:15 -0500

    Create randomnumgame.py

---
+++ randomnumgame.py
@@ -0,0 +1,21 @@
+import random
+
+number = random.randrange(1,11) 
+
+print(number)
+
+print("Guess the number. You'll get 5 tries.\nEnter a number from 1-10: ")
+
+usernum = int(input())
+
+count = 0
+while count<4:
+	if number != usernum:
+		print('not correct, try again')
+		usernum = int(input())
+		count += 1
+	elif number == usernum:
+		print('correct')
+		break
+else:
+	print('out of tries, number was', number)
